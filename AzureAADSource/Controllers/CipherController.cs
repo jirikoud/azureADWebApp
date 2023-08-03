89d739e3c59d4122806fb4f7ce6618253981d1de
jirikoud <koudelka@cyberfox.cz>
2023-08-03 15:14:54 +0200

    Upravy

--- AzureAADSource/Controllers/CipherController.cs
+++ AzureAADSource/Controllers/CipherController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using System.Buffers.Text;
 using System.IO;
 using System.Security.Cryptography;
+using System.Text.Unicode;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace AzureAADSource.Controllers
 {
@@ -16,13 +18,17 @@ namespace AzureAADSource.Controllers
         {
             try
             {
-                string privateAlice = "-----BEGIN PRIVATE KEY-----\r\nMIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQg5f/l0osQj9mV3kZU\r\npEKMK/iY5JplkHOAhvj7jzCyv/ShRANCAARswQJRb2eaJMKqi4BE9AZevwC7L1HU\r\nkMgrKssi3EixRifLwxWm+MtXUBKjIsd9E8YSZMhuDROr2v46P1dPk6rX\r\n-----END PRIVATE KEY-----";
+                string privateAlice = "-----BEGIN PRIVATE KEY-----MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQg5f/l0osQj9mV3kZUpEKMK/iY5JplkHOAhvj7jzCyv/ShRANCAARswQJRb2eaJMKqi4BE9AZevwC7L1HUkMgrKssi3EixRifLwxWm+MtXUBKjIsd9E8YSZMhuDROr2v46P1dPk6rX-----END PRIVATE KEY-----";
                 string privateBob = "-----BEGIN PRIVATE KEY-----\r\nMIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQggja4Ej9TkxtlcFTA\r\nddILSDDbx6FQTD5fd5jd9sg31guhRANCAAQoX+YTzy3AGr9cctHjQZ9MaSz2kk2f\r\nCd3mKMJ1N0BLvx4LLrw3r/GFlVXVQS4v78Lt+iyXQ3mUsm9znmhoEdly\r\n-----END PRIVATE KEY-----";
+                string cipheredText = "Srr23zad688FBpbTMbyjxKQ=";
+                string nonceText = "0zHvUkrFAGqlfp2K";
+                string tagText = "+ZSghUV/tbyJGVPXZ14TTg==";
 
                 //CngKey bobPrivateKey = CngKey.Import(Convert.FromBase64String(bobPrivate), CngKeyBlobFormat.GenericPrivateBlob);
                 //ECDiffieHellmanCng crypto = new ECDiffieHellmanCng(bobPrivateKey);
 
                 ECDiffieHellman cryptoAlice = new ECDiffieHellmanCng(ECCurve.CreateFromFriendlyName("secp256r1"));
+                var privateAliceKeyZero = cryptoAlice.ExportECPrivateKeyPem();
                 cryptoAlice.ImportFromPem(privateAlice);
                 var privateAliceKey = cryptoAlice.ExportECPrivateKeyPem();
                 var publicAliceKey = cryptoAlice.PublicKey;
@@ -37,8 +43,22 @@ namespace AzureAADSource.Controllers
 
                 //var memoryStream = new MemoryStream();
                 //memoryStream.Write(Convert.FromBase64String("XlsY5scsUiUeaCMgdDfUdTffiaGfkJboCyvZoImJ8blrH+ufwNvIkaAYQkvc"));
-                var derivedKey = HKDF.DeriveKey(HashAlgorithmName.SHA256, inKey, 32, new byte[0], new byte[0]);
+                var derivedKey = HKDF.DeriveKey(HashAlgorithmName.SHA256, inKey, 32, null, null);
                 //CryptoStream cryptStream = new CryptoStream(memoryStream, aes.CreateDecryptor(key, iv), CryptoStreamMode.Read);
+
+                derivedKey = Convert.FromBase64String("15zJkw8Dyr1w4iQZw92miip3CQBWlVkpiJUsZacdexs=");
+
+                var data = Convert.FromBase64String(cipheredText);
+                byte[] decryptedData = new byte[data.Length];
+                byte[] nonce = Convert.FromBase64String(nonceText);
+                byte[] tag = Convert.FromBase64String(tagText);
+
+                var crypto = new AesGcm(derivedKey);
+                crypto.Decrypt(nonce, data, tag, decryptedData, null);
+
+                var decryptedText = System.Text.Encoding.UTF8.GetString(decryptedData);
+
+
 
 
                 //using (FileStream fileStream = new("TestData.txt", FileMode.OpenOrCreate))
